0e9c9e0c471ee9152ab180af8d726ccd3db940f0
bakattsean <ttsean1999@gmail.com>
2020-12-24 10:07:55 +0800

    Update Card and added creditCardScene

--- Evergreen/src/sample/Card.java
+++ Evergreen/src/sample/Card.java
@@ -5,6 +5,9 @@ public class Card {
     public String cardID;
     public String name;
     public String expiryDate;
+    public String cvv;
+
+    public double fixedMonthlyLimit;
 
     public String getCardID() { return cardID; }
 
@@ -17,4 +20,32 @@ public class Card {
     public String getExpiryDate() { return expiryDate; }
 
     public void setExpiryDate(String expiryDate) { this.expiryDate = expiryDate; }
+
+    public String getCvv() { return cvv; }
+
+    public void setCvv(String cvv) { this.cvv = cvv; }
+
+    public double getFixedMonthlyLimit() {
+        return fixedMonthlyLimit;
+    }
+
+    public void setFixedMonthlyLimit(double fixedMonthlyLimit) {
+        this.fixedMonthlyLimit = fixedMonthlyLimit;
+    }
+
+    public boolean isValid(String amount) {
+        if (amount == null)
+            return false;
+
+        try {
+            double limitAmount = Double.parseDouble(amount);
+
+            if (limitAmount < 0)
+                return false;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
 }
