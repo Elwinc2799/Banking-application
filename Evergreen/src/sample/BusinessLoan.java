9fe182ef06bfc4f3ee1a230e72cdfc7965a51009
Eric Cheah <ericcheah575@gmail.com>
2020-12-24 08:52:49 +0800

    Change functions

--- Evergreen/src/sample/BusinessLoan.java
+++ Evergreen/src/sample/BusinessLoan.java
@@ -2,16 +2,28 @@ package sample;
 
 public class BusinessLoan extends Loan {
 
-    public double loanLimit;
-    public double[][] interestRate = { { 8.45, 9.35, 10.15 },
-                                        { 9.55, 10.65, 12.45 }
-    };
+    public String collateralType;
 
-    public BusinessCollateral businessCollateral;
+    public double collateralAmount;
+    public double[][] interestRate = { { 8.45, 9.35, 10.15 },
+            { 9.55, 10.65, 12.45 }
+    };
 
     public double[][] getInterestRate() { return interestRate; }
 
-    public double getLoanLimit() { return loanLimit; }
+    public String getCollateralType() {
+        return collateralType;
+    }
 
-    public void setLoanLimit(double loanLimit) { this.loanLimit = loanLimit; }
+    public void setCollateralType(String collateralType) {
+        this.collateralType = collateralType;
+    }
+
+    public double getCollateralAmount() {
+        return collateralAmount;
+    }
+
+    public void setCollateralAmount(double collateralAmount) {
+        this.collateralAmount = collateralAmount;
+    }
 }
