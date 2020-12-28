05d8db2cc2dd2763cf412cc30aab2cd9ef52ea9d
bakattsean <ttsean1999@gmail.com>
2020-12-28 20:49:00 +0800

    Add debitCardScene and update LoginController

--- Evergreen/src/sample/LoginController.java
+++ Evergreen/src/sample/LoginController.java
@@ -1,202 +1,66 @@
-package sample;
+<?xml version="1.0" encoding="UTF-8"?>
 
-import javafx.animation.FadeTransition;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import org.controlsfx.control.PopOver;
+        <?import javafx.scene.control.Button?>
+        <?import javafx.scene.control.Label?>
+        <?import javafx.scene.control.PasswordField?>
+        <?import javafx.scene.control.TextField?>
+        <?import javafx.scene.image.Image?>
+        <?import javafx.scene.image.ImageView?>
+        <?import javafx.scene.layout.AnchorPane?>
+        <?import javafx.scene.shape.Line?>
+        <?import javafx.scene.shape.Rectangle?>
+        <?import javafx.scene.text.Font?>
 
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class LoginController implements Initializable {
-
-    @FXML
-    private AnchorPane root;
-
-    @FXML
-    private TextField userName;
-
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private Button forgotPasswordButton;
-
-    TextField emailField = new TextField();
-    PasswordField newPasswordField = new PasswordField();
-
-    @FXML
-    public void login() {
-        /*boolean validated = false;
-
-        for (Map.Entry<String, String> row : ReadFile.passwordMap.entrySet()) {
-            String key = row.getKey();
-
-            if (key.equals(userName.getText().toLowerCase(Locale.ROOT))) && row.getValue().equals(passwordField.getText())) {
-                ReadFile.importData();
-                makeFadeOut();
-                ReadFile.passwordMap.clear();
-                validated = true;
-            }
-        }
-
-        if (!validated)
-            validation(); */
-
-        ReadFile.DataStorage.setUsername(userName.getText().toLowerCase(Locale.ROOT));
-        ReadFile.importData();
-        makeFadeOut();
-    }
-
-    @FXML
-    public void forgetPasswordPushed() {
-        Label emailLabel = new Label("Email");
-        emailLabel.setTranslateX(10);
-        emailLabel.setTranslateY(10);
-        emailLabel.setFont(new Font("Arial", 14));
-
-        emailField.setPromptText("Email");
-        emailField.setTranslateX(10);
-        emailField.setTranslateY(20);
-        emailField.setMaxWidth(200);
-        emailField.setFocusTraversable(false);
-        emailField.setFont(new Font("Arial", 12));
-
-        Button generateOTP = new Button();
-        generateOTP.setText("Generate OTP");
-        generateOTP.setTranslateX(125);
-        generateOTP.setTranslateY(70);
-
-        final VBox[] vBox = {new VBox(emailLabel, emailField, generateOTP)};
-        vBox[0].setPrefHeight(150);
-        vBox[0].setPrefWidth(250);
-
-        PopOver popOvers = new PopOver(vBox[0]);
-        popOvers.show(forgotPasswordButton);
-
-        generateOTP.setOnAction(actionEvent -> {
-            vBox[0].getChildren().clear();
-            TransferController instance = new TransferController();
-            ReadFile.DataStorage.savingsAccount.setEmail(emailField.getText());
-            new Thread(instance.sendOTPTask).start();
-
-            Label OTP = new Label("OTP");
-            Label newPassword = new Label("New password");
-            Label confirmPassword = new Label("Confirm password");
-            OTP.setTranslateX(10);
-            OTP.setTranslateY(10);
-            OTP.setFont(new Font("Arial", 14));
-            newPassword.setTranslateX(10);
-            newPassword.setTranslateY(50);
-            newPassword.setFont(new Font("Arial", 14));
-            confirmPassword.setTranslateX(10);
-            confirmPassword.setTranslateY(70);
-            confirmPassword.setFont(new Font("Arial", 14));
-
-            PasswordField otpField = new PasswordField();
-            PasswordField confirmPasswordField = new PasswordField();
-            otpField.setPromptText("OTP");
-            otpField.setTranslateX(10);
-            otpField.setTranslateY(0);
-            otpField.setMaxWidth(180);
-            otpField.setFocusTraversable(false);
-            newPasswordField.setTranslateX(10);
-            newPasswordField.setTranslateY(20);
-            newPasswordField.setMaxWidth(180);
-            newPasswordField.setFocusTraversable(false);
-            newPasswordField.setPromptText("New password");
-            confirmPasswordField.setTranslateX(10);
-            confirmPasswordField.setTranslateY(55);
-            confirmPasswordField.setMaxWidth(180);
-            confirmPasswordField.setFocusTraversable(false);
-            confirmPasswordField.setPromptText("Confirm Password");
-
-            Button nextButton = new Button();
-            nextButton.setText("Next");
-            nextButton.setTranslateX(155);
-            nextButton.setTranslateY(100);
-
-            vBox[0] = new VBox(OTP, newPassword, otpField, confirmPassword, newPasswordField, confirmPasswordField, nextButton);
-            vBox[0].setPrefHeight(250);
-            vBox[0].setPrefWidth(250);
-
-            PopOver popOver = new PopOver(vBox[0]);
-            popOver.show(forgotPasswordButton);
-
-            nextButton.setOnAction(actionEvent1 -> {
-                if (instance.getOTP().equals(otpField.getText()) && newPasswordField.getText().equals(confirmPasswordField.getText())) {
-                    try {
-                        Class.forName("oracle.jdbc.OracleDriver");
-                        Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "SYSTEM", ReadFile.accPassword);
-                        Statement statement = connect.createStatement();
-
-                        statement.executeQuery("UPDATE LOGIN SET PASSWORD = '" + newPasswordField.getText() +
-                                "' WHERE EMAIL = '" + emailField.getText() + "'");
-                    } catch (SQLException | ClassNotFoundException e) { e.printStackTrace(); }
-
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setContentText("You have successfully change your password. Please try and login with the new password now.");
-                    alert.showAndWait();
-                }
-                else {
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setContentText("Invalid OTP or password does not match");
-                    alert.showAndWait();
-                }
-            });
-
-        });
-    }
-
-    public void validation() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setContentText("Invalid email or password");
-        alert.showAndWait();
-    }
-
-    private void makeFadeOut() {
-        FadeTransition fadeTransition = new FadeTransition();
-        fadeTransition.setDuration((Duration.millis(1500)));
-        fadeTransition.setNode(root);
-        fadeTransition.setFromValue(1);
-        fadeTransition.setToValue(0);
-
-        fadeTransition.setOnFinished((ActionEvent event) -> loadNextScene());
-        fadeTransition.play();
-    }
-
-    private void loadNextScene() {
-        try {
-            Parent secondView;
-            secondView = FXMLLoader.load(getClass().getResource("accountScene.fxml"));
-            Scene newScene = new Scene(secondView);
-            Stage curStage = (Stage) root.getScene().getWindow();
-            curStage.setScene(newScene);
-        } catch (IOException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        new Thread(ReadFile.passwordValidationTask).start();
-    }
-}
+<AnchorPane fx:id="root" maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="700.0" prefWidth="1000.0" xmlns="http://javafx.com/javafx/11.0.1" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.LoginController">
+<Rectangle arcHeight="5.0" arcWidth="5.0" fill="WHITE" height="700.0" layoutX="525.0" stroke="WHITE" strokeType="INSIDE" width="475.0" />
+<ImageView fitHeight="700.0" fitWidth="525.0" pickOnBounds="true" preserveRatio="true">
+<image>
+<Image url="@Resources/derek-liang-MuDLzGMU06c-unsplash.jpg" />
+</image>
+</ImageView>
+<Label layoutX="637.0" layoutY="241.0" text="Sign In To Your Account">
+<font>
+<Font name="Candara Bold" size="24.0" />
+</font>
+</Label>
+<Line endX="172.39996337890625" layoutX="734.0" layoutY="326.0" startX="-100.0" />
+<Line endX="172.39996337890625" layoutX="734.0" layoutY="377.0" startX="-100.0" />
+<TextField fx:id="userName" focusTraversable="false" layoutX="633.0" layoutY="298.0" prefHeight="26.0" prefWidth="273.0" promptText="Email or Username" styleClass="custom-text-field-2" stylesheets="@sample.css">
+<font>
+<Font name="Arial Rounded MT Bold" size="14.0" />
+</font>
+</TextField>
+<PasswordField fx:id="passwordField" focusTraversable="false" layoutX="634.0" layoutY="350.0" promptText="Password" styleClass="custom-text-field-2" stylesheets="@sample.css">
+<font>
+<Font name="Arial Rounded MT Bold" size="14.0" />
+</font>
+</PasswordField>
+<Button layoutX="629.0" layoutY="426.0" mnemonicParsing="false" onAction="#login" prefHeight="18.0" prefWidth="131.0" styleClass="round-gray" stylesheets="@transparent.css" text="Login">
+<font>
+<Font name="System Bold" size="14.0" />
+</font>
+</Button>
+<Label layoutX="635.0" layoutY="111.0" text="Welcome to">
+<font>
+<Font name="Candara Bold" size="24.0" />
+</font>
+</Label>
+<Rectangle arcHeight="5.0" arcWidth="5.0" fill="#4a97df" height="3.0" layoutX="638.0" layoutY="205.0" stroke="#4a97df" strokeLineCap="ROUND" strokeLineJoin="ROUND" strokeMiterLimit="0.0" width="113.0" />
+<Rectangle arcHeight="5.0" arcWidth="5.0" fill="#d0d7dc" height="3.0" layoutX="761.0" layoutY="205.0" stroke="#d0d7dc" strokeLineCap="ROUND" strokeLineJoin="ROUND" strokeMiterLimit="0.0" width="113.0" />
+<ImageView fitHeight="236.0" fitWidth="304.0" layoutX="763.0" layoutY="464.0" pickOnBounds="true" preserveRatio="true">
+<image>
+<Image url="@Resources/OrganicLogo.png" />
+</image>
+</ImageView>
+<Label layoutX="636.0" layoutY="166.0" text="We make it wasy for everyone to maximize " textFill="GREY">
+<font>
+<Font name="Arial Bold" size="11.0" />
+</font>
+</Label>
+<Label layoutX="636.0" layoutY="180.0" text="their investment." textFill="GREY">
+<font>
+<Font name="Arial Bold" size="11.0" />
+</font>
+</Label>
+<Button fx:id="forgotPasswordButton" layoutX="803.0" layoutY="429.0" mnemonicParsing="false" onAction="#forgetPasswordPushed" stylesheets="@sample.css" text="Forget Password" textFill="#5a879a" underline="true" />
+</AnchorPane>
